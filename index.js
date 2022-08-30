1866024c7fc1b9c7fdef6a4a0e0eb4123f29d6db
chrisdennis12345@gmail.com <chrisdennis@Chriss-iMac.local>
2022-08-30 14:21:18 -0700

    finsihed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupBlock){
+    return Math.abs(42 - pickupBlock)
+}
+
+function distanceFromHqInFeet(pickupBlock){
+    return distanceFromHqInBlocks(pickupBlock) * 264
+}
+
+function distanceTravelledInFeet(start, end){
+    return Math.abs(start - end) * 264
+}
+
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination)
+    if (distance <=400){
+        return 0
+    } else if (distance <= 2000){
+        return (distance - 400) * 0.02
+    } else if (distance < 2500){
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+
+}
